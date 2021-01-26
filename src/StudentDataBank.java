e70d8566759e17f4a741517d7ea0255efc2b67f6
thomas.denezza <thomas.denezza@MU-C1-09.mullen.pvt.k12.co.us>
2021-01-26 10:07:17 -0700

    Added student object class, arraylist method, & text file of students

---
+++ src/StudentDataBank.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class StudentDataBank
+	{
+	public static void CompileArray() throws IOException
+		{
+		Scanner file = new Scanner(new File("StudentList.txt"));
+		
+		}
+	}
