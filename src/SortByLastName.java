530a1ae78ea9bbef72b3d6aa2eeedd9e1d7576cf
gabrielle.deorio <gabrielle.deorio@MU-C1-18.mullen.pvt.k12.co.us>
2021-01-28 11:03:18 -0700

    last name sort

--- src/SortByLastName.java
+++ src/SortByLastName.java
@@ -1,4 +1,7 @@
-
-public class SortByLastName {
-
+import java.util.Comparator;
+public class SortByLastName implements Comparator<Student> {
+public int compare (Student s1, Student s2)
+{
+	return s1.getLName().compareTo(s2.getLName());
+}
 }
