cf2055e46eb6d8bd3415df62df73110094f3aaa0
elledeorio <elledeorio@172-0-2-1.lightspeed.brhmal.sbcglobal.net>
2021-02-02 09:33:00 -0700

    sort by period

---
+++ src/SortByPeriod.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+public class SortByPeriod implements Comparator<Student>
+{
+	public int compare (Student s1, Student s2)
+	{
+		if (MainMenu.periodSort==1)
+			return s1.getClass1().compareTo(s2.getClass2());
+		else if (MainMenu.periodSort==2)
+			return s1.getClass2().compareTo(s2.getClass2());
+		else 
+			return s1.getClass3().compareTo(s2.getClass3());
+	}
+}
+
