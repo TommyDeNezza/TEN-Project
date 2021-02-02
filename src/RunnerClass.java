83bfc826a8677e1f848a19d8124b4d099a179fcd
thomas.denezza <thomas.denezza@MU-C1-09.mullen.pvt.k12.co.us>
2021-02-02 09:32:23 -0700

    Created Sorting GPA class

--- src/RunnerClass.java
+++ src/RunnerClass.java
@@ -5,7 +5,6 @@ public class RunnerClass
 	public static void main(String[] args) throws IOException
 		{
 		StudentDataBank.CompileArray();
-		StudentDataBank.generateGPA();
 		System.out.println(StudentDataBank.studentData.get(0).getGpa());
 		MainMenu.navigate();
 		}
