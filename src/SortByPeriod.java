a636aff9d0f3002fb3704b55c57ab574807f45fa
TommyDeNezza <thomasdenezza@gmail.com>
2021-02-16 20:02:20 -0700

    Edited and fixed things. Added looping and modified printing

--- src/SortByPeriod.java
+++ src/SortByPeriod.java
@@ -4,7 +4,7 @@ public class SortByPeriod implements Comparator<Student>
 	public int compare (Student s1, Student s2)
 	{
 		if (MainMenu.periodSort==1)
-			return s1.getClass1().compareTo(s2.getClass2());
+			return s1.getClass1().compareTo(s2.getClass1());
 		else if (MainMenu.periodSort==2)
 			return s1.getClass2().compareTo(s2.getClass2());
 		else 
